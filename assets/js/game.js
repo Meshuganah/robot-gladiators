cfc14001912e112ecddf18bcdf502b038f8a240e
Josh Klein <kleinjjosh74@gmail.com>
2022-02-26 08:55:41 -0500

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -72,6 +72,14 @@ var startGame = function() {
             var pickedEnemyName = enemyNames[i];
             enemyHealth = 50;
             fight(pickedEnemyName);
+
+            if (playerHealth > 0 && i < enemyNames.length - 1) {
+                var storeConfirm = window.confirm(`The fight is over, visit the store before the next round? `);
+
+                if (storeConfirm) {
+                    shop();
+                }
+            }
         } else {
             window.alert(`You have lost your robot in battle! Game Over!`);
             break;
@@ -93,6 +101,44 @@ var endGame = function() {
         startGame();
     } else {
         window.alert(`Thank you for playing Robot Gladiators! Come back soon!`)
+    }
+}
+
+var shop = function() {
+    var shopOptionPrompt = window.prompt(
+        `Would you like to REFILL your health, UPGRADE your attack, or LEAVE the shop?`
+    )
+
+    shopOptionPrompt = shopOptionPrompt.toLowerCase();
+    switch(shopOptionPrompt) {
+        case "refill":
+            if(playerMoney >= 7) {
+                window.alert("Refilling player's health by 20 for $7");
+                playerHealth = playerHealth + 20;
+                playerMoney = playerMoney - 7;
+            } else {
+                window.alert(`You don't have enough money!`)
+            }
+            break;
+
+        case "upgrade":
+            if(playerMoney >= 7) {
+                window.alert("Upgrading Player's attack by 6 for $7");
+                playerAttack = playerAttack + 6;
+                playerMoney = playerMoney - 7;
+            } else {
+                window.alert(`You don't have enough money!`)
+            }
+            break;
+
+        case "leave":
+            window.alert("Leaving the store");
+            break;
+
+        case "default": 
+            window.alert("You did not pick a valid option, please try again!");
+            shop();
+            break;
     }
 }
 
